ef4c81c781f116aec5d29fa1d8eeff2ffee403dc
Michelle Koeth <michelle@codeforamerica.org>
2012-02-25 18:16:03 -0500

    Basic rails setup

---
+++ uspto3ps/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uspto3ps::Application.config.secret_token = '63d58ad3418877cecaed45ea5aec301ec7c2440b5299aa4fe22623fdd8250e74e62eb8cdd4d11b83c017ea557bcae46e2f43de28707165cf5aee58c708a2a523'
