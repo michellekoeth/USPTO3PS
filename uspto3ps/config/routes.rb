b5a4271d37a9eeb8532df4afac201aa3ccbdd8cb
jerseycoder <michelle@codeforamerica.org>
2012-03-11 15:40:49 -0400

    Applied AGPLv3 License

--- uspto3ps/config/routes.rb
+++ uspto3ps/config/routes.rb
@@ -1,3 +1,19 @@
+#PatentBat - A web platform for facilitating the third party submissions of prior art under 37 CFR 1.290
+#Copyright (C) 2012 Michelle M. Koeth
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as
+#published by the Free Software Foundation, either version 3 of the
+#License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 Uspto3ps::Application.routes.draw do
   devise_for :users
   
